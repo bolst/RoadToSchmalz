fdd1afc6165a9e73735b5eab53e49279b7ba23db
nic <bolton21@uwindsor.ca>
2024-03-03 17:07:43 -0500

    update

--- RoadToSchmalz/obj/Debug/net6.0/RoadToSchmalz.AssemblyInfo.cs
+++ RoadToSchmalz/obj/Debug/net6.0/RoadToSchmalz.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
     " a component library built on top of Blazor and CSS frameworks like Bootstrap, T" +
     "ailwind, Bulma and Material.\n\t\t")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.3.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.3+8ebed7cf9164e39849aa642b8f42f6a70573878c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.3+55f3ddbc381fbfadac6cd3d6073696b76a6dc4b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoadToSchmalz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoadToSchmalz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.3.0.0")]
