55f3ddbc381fbfadac6cd3d6073696b76a6dc4b9
bolst <bolton21@uwindsor.ca>
2024-03-03 16:16:15 -0500

    update

--- RoadToSchmalz/obj/Debug/net6.0/RoadToSchmalz.AssemblyInfo.cs
+++ RoadToSchmalz/obj/Debug/net6.0/RoadToSchmalz.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
     " a component library built on top of Blazor and CSS frameworks like Bootstrap, T" +
     "ailwind, Bulma and Material.\n\t\t")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.3.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.3+61fc7c684a7171e5494857d719f8abf8c54f7f71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.3+8ebed7cf9164e39849aa642b8f42f6a70573878c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoadToSchmalz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoadToSchmalz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.3.0.0")]
--- RoadToSchmalz/obj/Release/net6.0/RoadToSchmalz.AssemblyInfo.cs
+++ RoadToSchmalz/obj/Release/net6.0/RoadToSchmalz.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
     "is a component library built on top of Blazor and CSS frameworks like Bootstrap," +
     " Tailwind, Bulma and Material.\r\n\t\t")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.3.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.3+da616b44ec145e251ded01675c521288e00720ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.3+8ebed7cf9164e39849aa642b8f42f6a70573878c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoadToSchmalz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoadToSchmalz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.3.0.0")]
