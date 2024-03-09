93d7abfa714f51580db6a072133dd5051f75c1ba
bolst <bolton21@uwindsor.ca>
2024-03-09 12:24:59 -0500

    fix matchups set as TBD breaking code

--- RoadToSchmalz/obj/Debug/net6.0/RoadToSchmalz.AssemblyInfo.cs
+++ RoadToSchmalz/obj/Debug/net6.0/RoadToSchmalz.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
     "is a component library built on top of Blazor and CSS frameworks like Bootstrap," +
     " Tailwind, Bulma and Material.\r\n\t\t")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.3.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.3+9b5d6777dd775a210aefdf928be68dc5f814f6fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.3+1e84c609d3d133e8516a92898f9d125a772c00c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoadToSchmalz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoadToSchmalz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.3.0.0")]
